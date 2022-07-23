4a86a593238cfa843db7d0feca59745b9acbccb2
zhanglingyu123 <zhangly@qq.com>
2022-07-23 18:15:25 +0800

    second commit

--- src/main/java/com/zhangly/git/GitTest.java
+++ src/main/java/com/zhangly/git/GitTest.java
@@ -3,5 +3,6 @@ package com.zhangly.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("hello, git");
     }
 }
